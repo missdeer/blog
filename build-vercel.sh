8e623163d91b388172ec8942cd4e6f8e0beeda6b
Fan Yang <missdeer@gmail.com>
2024-01-09 17:16:04 +0800

    install bundle & go on vercel

--- build-vercel.sh
+++ build-vercel.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [ ! -d plantuml-cmd ]; then
+	curl -o go.tar.gz https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+	tar xvf go.tar.gz
+	export PATH=%PATH%:$PWD/go/bin
 	git clone https://github.com/missdeer/plantuml-cmd.git
 	cd plantuml-cmd
 	go build
 	cd ..
 fi
+bundle install
 sed -i 's/\/usr\/local\/bin\/plantuml-cmd/\/vercel\/path0\/plantuml-cmd\/plantuml-cmd/g' _config.yml
 sed -i '/baseurl/d' _config.yml
 sed -i 's|site.config\[\x27baseurl\x27\] +||g' _plugins/ditaa.rb
